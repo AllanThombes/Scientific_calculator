efd6f5de969c84f3f54028746f0afa4cba2f1d45
thomas <thomas1.leblan@coding-academy.fr>
2016-08-22 17:38:59 +0200

    advanced functions added (sin, cos, tan, log, etc.) in Calc class

--- scientific_calc/lib/scientific_calc.rb
+++ scientific_calc/lib/scientific_calc.rb
@@ -21,6 +21,28 @@ class Calc
           then @@result = operation[0] % operation[2]
         when "^"
           then @@result = operation[0] ** operation[2]
+        else
+          return puts "Operation not recognized"
+      end
+      p @@result
+  end
+  # function which do the basical operations
+  def self.advanced(operation)
+      # we convert the second operand to float
+      operation[1] = operation[1].to_f
+      case operation[0]
+        when "cos"
+          then @@result = Math::cos(operation[1])
+        when "tan"
+          then @@result = Math::tan(operation[1])
+        when "sin"
+          then @@result = Math::sin(operation[1])
+        when "log"
+          then @@result = Math::log(operation[1])
+        when "exp"
+          then @@result = Math::exp(operation[1])
+        else
+          return puts "Operation not recognized"
       end
       p @@result
   end
