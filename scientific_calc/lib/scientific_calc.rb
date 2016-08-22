c74d7f68ae16203ab59e0786e0820c80539aa8bc
thomas <thomas1.leblan@coding-academy.fr>
2016-08-22 16:22:43 +0200

    First step: basic operations: + - * / % **  (scientific_calc.rb)

--- scientific_calc/lib/scientific_calc.rb
+++ scientific_calc/lib/scientific_calc.rb
@@ -1,12 +1,44 @@
-require "scienctific_calc/version"
+require "../lib/scientific_calc/version"
 
-module ScientificCalc
-  # Your code goes here...
+class Calc
+  @@result = 0
+
+  def self.basics(operation)
+      case operation[1]
+        when "+"
+          then @@result = operation[0] + operation[2]
+        when "-"
+          then @@result = operation[0] - operation[2]
+        when "*"
+          then @@result = operation[0] * operation[2]
+        when "/"
+          then @@result = operation[0] / operation[2]
+        when "mod"
+          then @@result = operation[0] % operation[2]
+        when "^"
+          then @@result = operation[0] ** operation[2]
+      end
+      p @@result
+  end
 end
 
+# ve wait operation from the user
+puts "Enter your calc: (type exit to quit)"
+command = gets
+while command.chomp != "exit" do
 
-class Calc < Math
+  # we split the user's entry with spaces
+  operation = command.chomp.split(" ")
+  if operation.length == 3
+      operation[0] = operation[0].to_f
+      operation[2] = operation[2].to_f
+      Calc.basics(operation)
+  end
 
-
-
+  # we wait the next operation
+  command = gets
 end
+
+# module ScientificCalc
+#   # Your code goes here...
+# end
--- scientific_calc/lib/scientific_calc/version.rb
+++ scientific_calc/lib/scientific_calc/version.rb
@@ -1,3 +1,3 @@
-module ScienctificCalc
+module ScientificCalc
   VERSION = "0.1.0"
 end
